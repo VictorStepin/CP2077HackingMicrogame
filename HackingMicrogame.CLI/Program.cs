bfa80de211f75636ea27cbfe0f980c7b33a77f09
Victor Stepin <vicstep1994@gmail.com>
2022-12-25 16:57:01 +0700

    Selecting code in row added.

--- HackingMicrogame.CLI/Program.cs
+++ HackingMicrogame.CLI/Program.cs
@@ -1,4 +1,64 @@
-﻿Console.WriteLine("1C 1C 1C 1C 1C");
+﻿bool isRunning = true;
+string[] matrixCodes = { "1C", "E9", "BD", "55" };
+string[] codeSequence = FillCodeSequence();
+int selectedCodeIndex = 0;
 
-Console.WriteLine("\nPress any key to exit..");
-Console.ReadKey(true);
+while (isRunning)
+{
+    Update();
+
+    var inputKey = Console.ReadKey().Key;
+    switch (inputKey)
+    {
+        case ConsoleKey.RightArrow:
+            if (selectedCodeIndex < codeSequence.Length - 1) selectedCodeIndex++;
+        break; 
+        case ConsoleKey.LeftArrow:
+            if (selectedCodeIndex > 0) selectedCodeIndex--;
+        break;
+        default:
+            isRunning = false;
+        break;
+    }
+}
+
+void Update()
+{
+    Console.Clear();
+    PrintCodeSequence(codeSequence);
+
+    Console.WriteLine("\nPress any key to exit..");
+}
+
+void PrintCodeSequence (string[] codeSequence)
+{
+    for (int i = 0; i < codeSequence.Length; i++)
+    {
+        string? code = codeSequence[i];
+        if (i == selectedCodeIndex)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"{code} ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        else
+        {
+            Console.Write($"{code} ");
+        }
+    }
+
+    Console.WriteLine();
+}
+
+string[] FillCodeSequence()
+{
+    string[] result = new string[5];
+    for (int i = 0; i < result.Length; i++)
+    {
+        int randomIndex = new Random().Next(0, matrixCodes.Length);
+        string randomCode = matrixCodes[randomIndex];
+        result[i] = randomCode;
+    }
+
+    return result;
+}
